5f882ed37089e3ed53e981d85f7a848b6c67ac0b
Xathor <andymarcel.97@gmail.com>
2021-02-07 20:40:15 +0100

    Add common Constants file.

---
+++ app/src/main/java/com/xathordroid/popcornroom/common/Constantes.kt
@@ -0,0 +1,14 @@
+package com.xathordroid.popcornroom.common
+
+class Constantes {
+
+    companion object {
+        public val TMDBAPI_BAASE_URL = "https://api.themoviedb.org/3/"
+        public val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500/"
+
+        public val API_KEY = ""
+
+        public val URL_PARAM_API_KEY = "api_key"
+        public val URL_PARAM_LANGUAGE = "language"
+    }
+}
