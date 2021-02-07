aa3ba0178cd8740fe661ae322082e1d088474bd9
Xathor <andymarcel.97@gmail.com>
2021-02-07 20:36:19 +0100

    Create a fragment-list to list the popular movies.

---
+++ app/src/main/java/com/xathordroid/popcornroom/ui/MovieFragment.kt
@@ -0,0 +1,61 @@
+package com.xathordroid.popcornroom.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.xathordroid.popcornroom.R
+import com.xathordroid.popcornroom.common.MyApp
+import com.xathordroid.popcornroom.data.Movie
+
+class MovieFragment : Fragment() {
+
+    private var columnCount = 1
+    private lateinit var moviesAdapter: MyMovieRecyclerViewAdapter
+    private var popularMovies: List<Movie> = ArrayList()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        arguments?.let {
+            columnCount = it.getInt(ARG_COLUMN_COUNT)
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_item_list, container, false)
+
+        moviesAdapter = MyMovieRecyclerViewAdapter(MyApp.getContext(), popularMovies)
+
+        // Set the adapter
+        if (view is RecyclerView) {
+            with(view) {
+                layoutManager = when {
+                    columnCount <= 1 -> LinearLayoutManager(context)
+                    else -> GridLayoutManager(context, columnCount)
+                }
+                adapter = moviesAdapter
+            }
+        }
+        return view
+    }
+
+    companion object {
+
+        // TODO: Customize parameter argument names
+        const val ARG_COLUMN_COUNT = "column-count"
+
+        // TODO: Customize parameter initialization
+        @JvmStatic
+        fun newInstance(columnCount: Int) =
+            MovieFragment().apply {
+                arguments = Bundle().apply {
+                    putInt(ARG_COLUMN_COUNT, columnCount)
+                }
+            }
+    }
+}
---
+++ app/src/main/java/com/xathordroid/popcornroom/ui/MyMovieRecyclerViewAdapter.kt
@@ -0,0 +1,33 @@
+package com.xathordroid.popcornroom.ui
+
+import android.content.Context
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.xathordroid.popcornroom.R
+import com.xathordroid.popcornroom.data.Movie
+
+class MyMovieRecyclerViewAdapter(private val context: Context, private val values: List<Movie>) : RecyclerView.Adapter<MyMovieRecyclerViewAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.fragment_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = values[position]
+    }
+
+    override fun getItemCount(): Int = values.size
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val idView: TextView = view.findViewById(R.id.item_number)
+        val contentView: TextView = view.findViewById(R.id.content)
+
+        override fun toString(): String {
+            return super.toString() + " '" + contentView.text + "'"
+        }
+    }
+}
