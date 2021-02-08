2b31f8b638b7a66c7b53214486a3db8d4df29045
Xathor <andymarcel.97@gmail.com>
2021-02-08 20:31:23 +0100

    Add missed decorator for ROOM DAO class.

--- app/src/main/java/com/xathordroid/popcornroom/data/local/dao/MovieDao.kt
+++ app/src/main/java/com/xathordroid/popcornroom/data/local/dao/MovieDao.kt
@@ -1,11 +1,13 @@
 package com.xathordroid.popcornroom.data.local.dao
 
 import androidx.lifecycle.LiveData
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.xathordroid.popcornroom.data.local.entity.Movie
 
+@Dao
 interface MovieDao {
 
     @Query("SELECT * FROM movies")
